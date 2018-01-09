0c430b222c4f88bc1087fe1c27bce73852c58c30
Ricardo Garcia <Richard_Grac@hotmail.com>
2018-01-08 19:07:18 -0600

    SecurityConfiguration added

---
+++ src/main/java/com/udemy/backendninja2/configuration/SecurityConfiguration.java
@@ -0,0 +1,53 @@
+package com.udemy.backendninja2.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration // Como es una clase de Configuración, agregamos esta notación
+@EnableWebSecurity  // Para habilitar la seguridad Web
+// WebSecurityConfigurerAdapter nos va a ayudar a guiarnos por la configuración
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    @Qualifier("userService")
+    private UserDetailsService userService;
+
+    // Nos servirá para añadir el UserDetailService que nos regresa el UserService
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        //passwordEncoder nos ayudará a encriptar la contraseña en la BD
+        auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    // Lo vamos a sobreescribir para la configuración
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // índicamos las Request Autorizadas sin la necesidad de un Login
+        http.authorizeRequests()
+                .antMatchers("/css/*", "/imgs/*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").loginProcessingUrl("/logincheck")
+                .usernameParameter("username").passwordParameter("password")
+                .defaultSuccessUrl("/loginsuccess").permitAll()
+                .and()
+                .logout().logoutUrl("/logout").logoutSuccessUrl("/login?logout").permitAll();
+
+        //Usamos el objeto http
+        // .antMatchers(): índicamos las Request autorizadas sin la necesidad de un login
+        // .anyRequest(): el resto de Request, las ponemos con Identificación
+        // .and(): añadimos más configuración
+        /* .formLogin(): índicamos la url del login page '/login' con la url que lo procesa '/logincheck'
+             con username y password. */
+        // defaultSuccessUrl() nos índica hacia qué url mandarnos al autentificarse. Y al login se lo permitimos a todos
+        // .and(): añadimos más configuración
+        // Lo mismo con el logout, se irá a login con el parámetro logout y un permitAll()
+    }
+}
